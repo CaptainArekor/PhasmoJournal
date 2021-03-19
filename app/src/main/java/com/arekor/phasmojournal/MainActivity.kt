e2d38d08aea49478f453670d0c0aefa381774c59
Arekor <bunner.benjamin@gmail.com>
2021-03-19 20:40:45 +0100

    Add sharedPreferenceUtils + Store locale

--- app/src/main/java/com/arekor/phasmojournal/MainActivity.kt
+++ app/src/main/java/com/arekor/phasmojournal/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.arekor.phasmojournal
 
-import android.content.SharedPreferences
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -8,23 +7,28 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.arekor.phasmojournal.utils.misc.SettingUtils
 import com.arekor.phasmojournal.utils.storage.SharedPreferencesUtils
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadStoredLocale()
+        setNavigation()
+    }
+
+    private fun loadStoredLocale() {
+        setLocale(SharedPreferencesUtils.getLocalePreference(this))
+    }
+
+    private fun setNavigation() {
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
         navView.setupWithNavController(navController)
-        val storedLocale = SharedPreferencesUtils.getLocalePreference(this)
-        setLocale(storedLocale)
     }
 
     fun setLocale(locale_text: String) {
         SettingUtils.setLocale(this,  locale_text)
-        SharedPreferencesUtils.setLocalePreference(this, locale_text)
     }
 
     fun getLanguageList() : Array<String> = SettingUtils.getLanguageList(this)
--- app/src/main/java/com/arekor/phasmojournal/utils/misc/SettingUtils.kt
+++ app/src/main/java/com/arekor/phasmojournal/utils/misc/SettingUtils.kt
@@ -2,27 +2,30 @@ package com.arekor.phasmojournal.utils.misc
 
 import android.content.Context
 import com.arekor.phasmojournal.R
+import com.arekor.phasmojournal.utils.storage.SharedPreferencesUtils
 import java.util.*
 
 object SettingUtils {
-    fun getLanguageList(context: Context) : Array<String> = arrayOf(
+    fun getLanguageList(context: Context): Array<String> = arrayOf(
         context.getString(R.string.locale_display_en),
         context.getString(R.string.locale_display_fr)
     ).sortedArray()
 
-    fun setLocale(context: Context, locale_text: String){
+    fun setLocale(context: Context, locale_text: String) {
         val locale = Locale(locale_text)
         val resources = context.resources
         val config = resources.configuration
         val displayMetrics = resources.displayMetrics
         config.setLocale(Locale(locale_text.toLowerCase(Locale.ROOT)))
+        config.setLayoutDirection(Locale(locale_text.toLowerCase(Locale.ROOT)))
         Locale.setDefault(locale)
         context.createConfigurationContext(config)
         resources.updateConfiguration(config, displayMetrics)
+        SharedPreferencesUtils.setLocalePreference(context, locale_text)
     }
 
-    fun getDisplayLanguage(context: Context, locale: Locale) : String {
-        return when(locale.language) {
+    fun getDisplayLanguage(context: Context, locale: Locale): String {
+        return when (locale.language) {
             context.resources.getString(R.string.locale_en) -> {
                 context.resources.getString(R.string.locale_display_en)
             }
