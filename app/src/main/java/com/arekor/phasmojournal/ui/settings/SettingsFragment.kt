a402888e674429a9cc58176ef084501f06f0bfaf
Arekor <bunner.benjamin@gmail.com>
2021-03-26 20:18:50 +0100

    Add toolbar effect

--- app/src/main/java/com/arekor/phasmojournal/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/arekor/phasmojournal/ui/settings/SettingsFragment.kt
@@ -8,7 +8,6 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.TextView
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.arekor.phasmojournal.MainActivity
 import com.arekor.phasmojournal.R
